cac5e0314a4d71784c96f346a83932e06ae3ce19
Danny <dannybellion@gmail.com>
2025-09-04 19:12:35 +0100

    Revert "Update dependencies in package.json and package-lock.json, add calendar tool to Sanity configuration, and include raceType in schemaTypes."
    This reverts commit 4b217bc5446f946e6c771e513d7cb432e7df3f7e.

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-import {calendarTool} from './plugins/calendar-tool'
 
 export default defineConfig({
   name: 'default',
@@ -13,7 +12,7 @@ export default defineConfig({
   
   basePath: '/',
 
-  plugins: [structureTool(), visionTool(), calendarTool()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,5 @@
 import {articleType} from './articleType'
 import {authorType} from './authorType'
 import {categoryType} from './categoryType'
-import {raceType} from './raceType'
 
-export const schemaTypes = [articleType, authorType, categoryType, raceType]
+export const schemaTypes = [articleType, authorType, categoryType]
